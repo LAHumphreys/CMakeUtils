9b8d426384197cf3f1b2c2b66e70c7d020aeca50
Luke Humphreys <lhumphreys67@gmail.com>
2018-01-02 13:56:04 +0000

    Script to do a coveralls run

---
+++ travis/doCoverage.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+set -evx
+
+# if possible, ask for the precise number of processors,
+# otherwise take 2 processors as reasonable default; see
+# https://docs.travis-ci.com/user/speeding-up-the-build/#Makefile-optimization
+if [ -x /usr/bin/getconf ]; then
+    NPROCESSORS=$(/usr/bin/getconf _NPROCESSORS_ONLN)
+else
+    NPROCESSORS=2
+fi
+
+# Tell make to use the processors. No preceding '-' required.
+MAKEFLAGS="j${NPROCESSORS}"
+export MAKEFLAGS
+
+env | sort
+
+# Generate the build tree
+mkdir Coverage || true
+cd Coverage
+cmake -DCMAKE_CXX_FLAGS=$CXX_FLAGS \
+      -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+      ..
+
+# Build the Code
+make
+
+# Run the tests
+ctest -T Memcheck
+
+# Post the coveralls result
+cd ..
+coveralls -r . -b Coverage -e dep -e Build -e test --gcov gcov-6 --verbose 
