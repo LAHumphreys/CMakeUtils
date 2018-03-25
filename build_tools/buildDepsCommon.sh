10bf6df55c15ff3b4f39116e3dea1600706aa0ae
Luke Humphreys <lhumphreys67@gmail.com>
2018-03-25 03:46:03 +0100

    Support -nouild on buildDeps
    This new mode is to support the build machine doing an initial retrieval
    from github, before applying local overrides

--- build_tools/buildDepsCommon.sh
+++ build_tools/buildDepsCommon.sh
@@ -1,3 +1,11 @@
+DEPS_FLAGS=""
+doBuild=true
+if [[ "$1" == "-nobuild" ]]; then
+    doBuild=false
+    DEPS_FLAGS+="-nobuild"
+    shift
+fi
+
 DEPS_BUILD=$PWD/deps/build
 if [[ "$1" == "" ]]; then
     echo "No install directory provided, falling back to: $DEPS_BUILD"
@@ -39,15 +47,19 @@ for dep in ${!depList[@]}; do
     git pull
 
     if [[ -e buildDeps.sh ]]; then
-        ./buildDeps.sh $DEPS_BUILD || exit 1
+        ./buildDeps.sh $DEPS_FLAGS $DEPS_BUILD || exit 1
     fi
 
-    pushd build || exit 1
 
-    cmake -DCMAKE_BUILD_TYPE=Release "-DCMAKE_PREFIX_PATH:PATH=$DEPS_CMAKE_DEPO" "-DCMAKE_INSTALL_PREFIX:PATH=$DEPS_BUILD" .. || exit 1
-    make -j 3 || exit 1
-    make install || exit 1
+    if [[ $doBuild == true ]]; then
+        pushd build || exit 1
 
-    popd || exit 1
+        cmake -DCMAKE_BUILD_TYPE=Release "-DCMAKE_PREFIX_PATH:PATH=$DEPS_CMAKE_DEPO" "-DCMAKE_INSTALL_PREFIX:PATH=$DEPS_BUILD" .. || exit 1
+        make -j 3 || exit 1
+        make install || exit 1
+
+        popd || exit 1
+    fi
+
     popd || exit 1
 done
