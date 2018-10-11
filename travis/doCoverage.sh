d8a51bb23c7f26c21ad2f5d8d5d2325a833c4e7d
Luke Humphreys <lhumphreys67@gmail.com>
2018-10-11 22:14:31 +0100

    Fixup CMAKE_PREFIX_PATH

--- travis/doCoverage.sh
+++ travis/doCoverage.sh
@@ -32,9 +32,10 @@ if [[ "$DEPS_ROOT" == "" ]]; then
     DEPS_ROOT=$PWD/deps
     echo "No deps directory (DEPS_ROOT) provided, falling back to: $DEPS_ROOT"
 fi
+DEPS_PREFIX="$DEPS_ROOT/build"
 DEPS_LOCATION="$DEPS_ROOT/build/lib/cmake"
 if [[ -e $DEPS_LOCATION ]]; then
-    DEPS_FLAGS="-DCMAKE_PREFIX_PATH:PATH=$DEPS_LOCATION -DCMAKE_MODULE_PATH:PATH=$DEPS_LOCATION"
+    DEPS_FLAGS="-DCMAKE_PREFIX_PATH:PATH=$DEPS_PREFIX -DCMAKE_MODULE_PATH:PATH=$DEPS_LOCATION"
 else
     DEPS_FLAGS=""
 fi
